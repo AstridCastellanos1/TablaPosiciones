ca5e40f1dc0394279aa073bc3c97375bdef231b6
AstridCastellanos1 <acastellanosc2@miumg.edu.gt>
2022-02-21 21:59:46 -0600

    proyecto c#

---
+++ CalcularTablaPosicionesC/CalcularTablaPosicionesC/Col.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularTablaPosicionesC
+{
+    internal class Col
+    {
+        public static readonly int NOMBRE = 1;
+        public static readonly int JUEGOS = 2;
+        public static readonly int PUNTOS = 3;
+        public static readonly int GANADOS = 4;
+        public static readonly int EMPATES = 5;
+        public static readonly int PERDIDOS = 6;
+        public static readonly int GOLES_FAVOR = 7;
+        public static readonly int GOLES_CONTRA = 8;
+        public static readonly int GOLES_DIFERENCIA = 9;
+    }
+}
